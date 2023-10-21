2ca44504fd96274a44097149d0b6c947d5381d6a
Anna <annapetrovic@gmx.ch>
2023-10-21 23:05:40 +0200

    added new file task 2.1.3

---
+++ task_score.js
@@ -0,0 +1,12 @@
+let introduction = 200;
+let git = 250;
+let js = 400;
+let total = introduction + git + js;
+let averageScore = total / 3;
+console.log(averageScore);
+
+let firstName = "Anna";
+let surName = "Petrovic";
+let fullName = firstName + " " + surName;
+console.log(fullName);
+console.log(`Student ${fullName} had ${total} scores.`);
