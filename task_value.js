fe0bb3c2b09c08c863b26b9bdef43e09ba19b8ee
Anna <annapetrovic@gmx.ch>
2023-10-22 10:11:52 +0200

    added file task 2.1.5

---
+++ task_value.js
@@ -0,0 +1,11 @@
+let a=48
+let b= Math.random()
+let bMax=Math.random(Math.max(b))
+b=Math.floor(Math.random() * bMax)
+console.log(a+b/5+17)
+/*
+сначала произошло деление
+затем первое сложение
+и в конце еще одно сложение
+т.о. приоритет операторов в JS соответствует правилам математики.
+*/
