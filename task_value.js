79400fabc61f46a59ccf97457dcd9a0f2df40f25
Anna <annapetrovic@gmx.ch>
2023-10-22 10:12:48 +0200

    fixed file task 2.1.5

--- task_value.js
+++ task_value.js
@@ -1,8 +1,8 @@
-let a=48
-let b= Math.random()
-let bMax=Math.random(Math.max(b))
-b=Math.floor(Math.random() * bMax)
-console.log(a+b/5+17)
+let a = 48;
+let b = Math.random();
+let bMax = Math.random(Math.max(b));
+b = Math.floor(Math.random() * bMax);
+console.log(a + b / 5 + 17);
 /*
 сначала произошло деление
 затем первое сложение
