a9ddc097b7f44a14374817a78c194f90ccbae38f
Anna <annapetrovic@gmx.ch>
2023-10-29 01:09:32 +0200

    added new file 2.3.1.1

---
+++ scorePoints_2.3.1.1.js
@@ -0,0 +1,41 @@
+const players = [
+    {
+    "id": 1,
+    "name": "Ivan",
+    "scorePoints": 4500
+    },
+    {
+    "id": 2,
+    "name": "Petr",
+    "scorePoints": 3600
+    },
+    {
+    "id": 3,
+    "name": "Vadim",
+    "scorePoints": 3433
+    },
+    {
+    "id": 4,
+    "name": "Olga",
+    "scorePoints": 2356
+    },
+    
+    ];
+
+    const scorePointsArr = [];
+    console.log(players[0].scorePoints)
+
+    for (let i=0; i<players.length; i++) {
+        console.log(players[i].scorePoints)
+
+    }
+    
+    for (player in players) {
+        scorePointsArr.push(players[player].scorePoints);
+    }
+
+    console.log(Math.max.apply(null,scorePointsArr))
+
+    const maxScorePoints ={...players, ...players, ...Math.max.apply(null,scorePointsArr)}
+    
+    console.log(maxScorePoints)
